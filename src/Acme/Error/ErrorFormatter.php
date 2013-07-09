db8936ebca87fd80efba18727bb00d7ba52a5a80
k-holy <k.holy74@gmail.com>
2013-07-09 18:06:21 +0900

    エラーフォーマッタの不要機能を削除

--- src/Acme/Error/ErrorFormatter.php
+++ src/Acme/Error/ErrorFormatter.php
@@ -16,14 +16,8 @@ namespace Acme\Error;
 class ErrorFormatter
 {
 
-	/* @const string エラーレベル */
-	const ERROR   = 'error';
-	const WARNING = 'warning';
-	const NOTICE  = 'notice';
-	const INFO    = 'info';
-
 	/* @var array PHPエラーレベル */
-	private static $errorLabels = array(
+	private static $errorLevels = array(
 		E_ERROR             => 'Fatal error',
 		E_WARNING           => 'Warning',
 		E_NOTICE            => 'Notice',
@@ -37,11 +31,6 @@ class ErrorFormatter
 	);
 
 	/**
-	 * @var TraceFormatterInterface トレースフォーマッタ
-	 */
-	private $traceFormatter;
-
-	/**
 	 * エラー情報を文字列に整形して返します。
 	 *
 	 * @param int エラーレベル
@@ -53,40 +42,14 @@ class ErrorFormatter
 	public function format($errno, $errstr, $errfile, $errline)
 	{
 		return sprintf("%s[%d]: '%s' in %s on line %d",
-			(isset(static::$errorLabels[$errno]))
-				? static::$errorLabels[$errno]
+			(isset(static::$errorLevels[$errno]))
+				? static::$errorLevels[$errno]
 				: 'Unknown error',
 			$errno,
 			$errstr,
 			$errfile,
 			$errline
 		);
-	}
-
-	/**
-	 * PHPエラー定数値をこのクラスのエラーレベル定数に変換して返します。
-	 *
-	 * @int PHPエラー定数
-	 */
-	public function convertErrorLevel($errno)
-	{
-		switch ($errno) {
-		case E_NOTICE:
-		case E_USER_NOTICE:
-			return self::NOTICE;
-		case E_WARNING:
-		case E_USER_WARNING:
-			return self::WARNING;
-		case E_ERROR:
-		case E_USER_ERROR:
-		case E_RECOVERABLE_ERROR:
-			return self::ERROR;
-		case E_STRICT:
-		case E_DEPRECATED:
-		case E_USER_DEPRECATED:
-		default:
-			return self::INFO;
-		}
 	}
 
 }
